b2fb96959d6780bde72b6a9a4ce84c2382cb312a
Zakaria Asad <zasad2587@gmail.com>
2024-01-12 00:46:31 +0200

    Building the Fotter component

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,11 +1,13 @@
 import React, { PropsWithChildren } from "react";
 import NavBar from "./_components/NavBar";
+import Footer from "./_components/Footer";
 
 const layout = ({ children }: PropsWithChildren) => {
   return (
     <div className="h-full bg-slate-100">
       <NavBar />
       <main className="pt-40 pb-20 bg-slate-100">{children}</main>
+      <Footer />
     </div>
   );
 };
