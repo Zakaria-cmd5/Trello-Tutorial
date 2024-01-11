cb98671e70dfe6bef442f1f32aa8450896edf69b
Zakaria Asad <zasad2587@gmail.com>
2024-01-12 00:38:46 +0200

    Build the NavBar components

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,8 +1,10 @@
 import React, { PropsWithChildren } from "react";
+import NavBar from "./_components/NavBar";
 
 const layout = ({ children }: PropsWithChildren) => {
   return (
     <div className="h-full bg-slate-100">
+      <NavBar />
       <main className="pt-40 pb-20 bg-slate-100">{children}</main>
     </div>
   );
