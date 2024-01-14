df1f183880cd232431d799d9656d4ad795f7ff03
Zakaria Asad <zasad2587@gmail.com>
2024-01-14 15:14:18 +0200

    Fetching the board from data base

---
+++ actions/createBoard.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import prisma from "@/prisma/client";
+import { z } from "zod";
+
+const createBoardSchema = z.object({
+  title: z.string().min(1).max(255),
+});
+
+export async function create(formData: FormData) {
+  const { title } = createBoardSchema.parse({
+    title: formData.get("title"),
+  });
+
+  await prisma.board.create({
+    data: {
+      title,
+    },
+  });
+}
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,29 @@
-const OrganizationIdPage = () => {
-  return (
-    <div>
-      OrganizationIdPage
-    </div>
-  )
-}
+import { create } from "@/actions/createBoard";
+import { Button } from "@/components/ui/button";
+import prisma from "@/prisma/client";
 
-export default OrganizationIdPage
+const OrganizationIdPage = async () => {
+  const board = await prisma.board.findMany();
+
+  return (
+    <div className="flex flex-col space-y-4">
+      <form action={create}>
+        <input
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a board title"
+          className="border-black border p-1"
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+      <div className="space-y-2">
+        {board.map((board) => (
+          <div key={board.id}>{board.title}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default OrganizationIdPage;
