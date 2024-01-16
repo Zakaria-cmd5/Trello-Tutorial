38f90ff50ef205c14c36b94dcb60a60828793a38
Zakaria Asad <zasad2587@gmail.com>
2024-01-16 18:12:55 +0200

    Removing the test form

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,18 +1,10 @@
-import prisma from "@/prisma/client";
-import Board from "./Board";
-import Form from "./Form";
+import Info from "./_components/Info";
 
 const OrganizationIdPage = async () => {
-  const board = await prisma.board.findMany();
 
   return (
-    <div className="flex flex-col space-y-4">
-      <Form />
-      <div className="space-y-2">
-        {board.map((board) => (
-          <Board id={board.id} title={board.title} key={board.id} />
-        ))}
-      </div>
+    <div className="w-full mb-20">
+     <Info />
     </div>
   );
 };
