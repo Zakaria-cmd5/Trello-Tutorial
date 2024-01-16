5b5e5d4efb79494ff2cf7855f2b2bc8deae8e84f
Zakaria Asad <zasad2587@gmail.com>
2024-01-16 15:38:26 +0200

    Building FormErrors component

--- components/form/FormInput.tsx
+++ components/form/FormInput.tsx
@@ -5,6 +5,7 @@ import { useFormStatus } from "react-dom";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
+import FormError from "./FormError";
 
 interface Props {
   id: string;
@@ -63,6 +64,7 @@ const FormInput = forwardRef<HTMLInputElement, Props>(
             aria-describedby={`${id}-error`}
           />
         </div>
+        <FormError id={id} errors={errors} />
       </div>
     );
   }
